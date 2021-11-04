5183c6074e6285b80765784e4daab629d44051a9
Brabados <rhysipod@gmail.com>
2021-11-05 04:50:57 +1100

    Beginings of grid and inital game peices. Will set up for model and mats.

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GridController : MonoBehaviour
+{
+    GamePiece[,] Board;
+    int Xsize;
+    int Ysize;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
