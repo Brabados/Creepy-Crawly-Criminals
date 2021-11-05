3e876aadc291924183bfc6d955f66fed68274f44
Brabados <rhysipod@gmail.com>
2021-11-06 03:06:57 +1100

    Basic prefabs prepared, need to remeber to change them. Basic colour materials added. GridController almost prepared to spawn peices; though not sure of scale and camera position yet.

--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class GamePiece : MonoBehaviour
 {
+
+    public int XPos;
+    public int YPos;
+
+    public bool moveable;
+    public bool clearable;
+
+
+
+    GamePiece(int x, int y)
+    {
+
+    }
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -8,10 +8,50 @@ public class GridController : MonoBehaviour
     GamePiece[,] Board;
     int Xsize;
     int Ysize;
+
+    public enum Type
+    {
+        ANT,
+        GRASSHOPPER,
+        FLY,
+        APHID,
+        ANY,
+        COUNT
+    }
+    public enum Colour
+    {
+        RED,
+        GREEN,
+        BLUE,
+        YELLOW,
+        ANY,
+        COUNT
+    }
+
+    private Dictionary<Type, GameObject> TypeDictionary;
+    private Dictionary<Colour, Material> ColourDictionary;
+
+    [System.Serializable]
+    public struct GamePeiceTypePrefabs
+    {
+        public Type type;
+        public GameObject Prefab;
+    }
+
+    [System.Serializable]
+    public struct GamePeiceColourPrefabs
+    {
+        public Colour type;
+        public Material Prefab;
+    }
+
+    public GamePeiceTypePrefabs[] _GamePeiceTypePrefabs;
+    public GamePeiceColourPrefabs[] _GamePeiceColourPrefabs;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        Board = new GamePiece[Xsize, Ysize];
         
     }
 
