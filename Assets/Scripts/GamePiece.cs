a3e71968c10e13b3c286931af20fc45f01ef0655
Brabados <rhysipod@gmail.com>
2021-11-07 18:35:53 +1100

    Whats completed before work

--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -5,18 +5,40 @@ using UnityEngine;
 public class GamePiece : MonoBehaviour
 {
 
-    public int XPos;
-    public int YPos;
+    private int _Xpos;
+    private int _Ypos;
+    private GridController _Grid;
+    private GridController.Type _Type;
+    private GridController.Colour _Colour;
+
+    public int XPos
+    {
+        get { return _Xpos; }
+    }
+    public int YPos
+    {
+        get { return _Ypos; }
+    }
+
+    public GridController Grid
+    {
+        get { return _Grid; }
+    }
+
+    public GridController.Type Type
+    {
+        get { return _Type; }
+    }
+
+    public GridController.Colour Colour
+    {
+        get { return _Colour; }
+    }
+
 
     public bool moveable;
     public bool clearable;
 
-
-
-    GamePiece(int x, int y)
-    {
-
-    }
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +49,14 @@ public class GamePiece : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Initalize(int x, int y, GridController grid, GridController.Type type ,GridController.Colour colour)
+    {
+        _Xpos = x;
+        _Ypos = y;
+        _Grid = grid;
+        _Type = type;
+        _Colour = colour;
     }
 }
